defea7cd437c5430753d141304ad87b1bc8ec0dd
Romeeros <romeeronayotama3@gmail.com>
2026-07-28 11:20:46 +0700

    feat: create project structure and navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">
-        Attendance System
-      </h1>
-    </main>
+    <>
+      <Navbar />
+
+      <main className="flex min-h-[calc(100vh-64px)] items-center justify-center">
+        <h2 className="text-4xl font-bold">
+          Welcome to Attendance System
+        </h2>
+      </main>
+    </>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="bg-blue-600 text-white px-6 py-4">
+      <h1 className="text-xl font-bold">Attendance System</h1>
+    </nav>
+  );
+}
